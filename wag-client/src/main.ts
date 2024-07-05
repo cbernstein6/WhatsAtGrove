506e81fc89d91e7d8b632e4fe23226714290920b
Chad Bernstein <chaddbernstein@gmail.com>
2024-07-05 14:16:17 -0400

    routing config + header starters

--- wag-client/src/main.ts
+++ wag-client/src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig)
+const config = {...appConfig, providers: [provideRouter(routes)]};
+bootstrapApplication(AppComponent, config)
   .catch((err) => console.error(err));
